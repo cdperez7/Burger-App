35d7bfe879576efbbb7c1a9444531d4b688b0508
Connor Perez <connorperez@gmail.com>
2019-06-18 09:29:40 -0500

    code in but not connecting

--- config/orm.js
+++ config/orm.js
@@ -6,6 +6,20 @@ var orm = {
             if (err) throw err;
             cb(result)
         })
+    },
+
+    update: function (tableInput, condition, cb) {
+        connection.query('UPDATE' + tableInput + 'SET devoured=true WHERE id=' + condition + ';', function (err, result) {
+            if (err) throw err;
+            cb(result);
+        })
+    },
+
+    create: function (table, val, cb) {
+        connection.query('INSERT INTO' + tableInput + " (burger_name) VALUES + ('"+ val +"');", function (err, result) {
+            if (err) throw err;
+            cb(result);
+        })
     }
 }
 
